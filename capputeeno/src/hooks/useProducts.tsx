e9ee3dcf254b5d4ed13f30c1a9333a02e4cd606b
codercly <rrclementirafael@gmail.com>
2023-06-29 16:56:21 -0400

    feat: initial products fetch

---
+++ capputeeno/src/hooks/useProducts.tsx
@@ -0,0 +1,35 @@
+
+import { useQuery } from '@tanstack/react-query';
+import { ProductsFetchResponse } from "@/types/products-response";
+
+import axios, { AxiosPromise } from 'axios';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+
+const fetcher = (): AxiosPromise<ProductsFetchResponse> => {
+  return axios.post(
+    API_URL, 
+    {
+      query: `query{
+          allProducts{
+            id
+            name
+            price_in_cents
+            image_url
+          }
+        }
+      `
+    })
+}
+
+export function useProducts() {
+  const { data } = useQuery({
+    queryFn: fetcher,
+    queryKey: ['products']
+  })
+
+  return {
+    //acessando diretamente o valor dos produtos
+    data: data?.data?.data?.allProducts
+  }
+}
